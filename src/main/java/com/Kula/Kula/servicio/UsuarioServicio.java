6fca35aedc1e030a8df08bed23671e0a48428841
Fabricio <fgrosso.04@gmail.com>
2020-02-24 13:19:50 -0300

    UsuarioServicio login / UsuarioRepositorio buscarporAliasoMail agregado application.properties agregado metodo index en portal controllador

--- src/main/java/com/Kula/Kula/servicio/UsuarioServicio.java
+++ src/main/java/com/Kula/Kula/servicio/UsuarioServicio.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
@@ -20,21 +21,21 @@ public class UsuarioServicio {
     private FotoServicio fotoServicio;
 
     @Transactional
-    public void guardarUsuario(MultipartFile archivo,String alias, String mail, String clave) throws ErrorServicio {
-        
-        validar(alias,mail, clave);
+    public void guardarUsuario(MultipartFile archivo, String alias, String mail, String clave) throws ErrorServicio {
+
+        validar(alias, mail, clave);
 
         Usuario usuario = new Usuario();
 
         usuario.setAlias(alias);
         usuario.setMail(mail);
         usuario.setContraseña(clave);
-        Foto foto = fotoServicio.guardar(archivo); 
+        Foto foto = fotoServicio.guardar(archivo);
         usuario.setFoto(foto);
         usuario.setEstado(true);
 
         usuarioRepositorio.save(usuario);
-        
+
     }
 
     @Transactional
@@ -59,29 +60,49 @@ public class UsuarioServicio {
     }
 
     @Transactional
-    public void eliminarUsuario(String id) throws ErrorServicio{
+    public void eliminarUsuario(String id) throws ErrorServicio {
         Optional<Usuario> respuesta = usuarioRepositorio.findById(id);
         if (respuesta.isPresent()) {
             Usuario usuario = respuesta.get();
             usuario.setEstado(false);
-            
+
             usuarioRepositorio.save(usuario);
-        }else{
+        } else {
             throw new ErrorServicio("No existe usuario con ese id.");
         }
     }
-    
+
     private void validar(String alias, String mail, String clave) throws ErrorServicio {
-        if (alias == null || alias.isEmpty()){
+        if (alias == null || alias.isEmpty()) {
             throw new ErrorServicio("El Alias del usuario no puede ser nulo.");
         }
-        
-        if (mail == null || mail.isEmpty()){
+
+        if (mail == null || mail.isEmpty()) {
             throw new ErrorServicio("El Mail del usuario no puede ser nulo.");
         }
-        
-        if (clave == null || clave.isEmpty() || clave.length() < 6){
+
+        if (clave == null || clave.isEmpty() || clave.length() < 6) {
             throw new ErrorServicio("La clave no puede ser nula y debe tener 6 caracter como minimo.");
+        }
+    }
+
+    //LOGICA DEL LOGIN RECIBE ALIAS/MAIL y CONTRASEÑA, recupera en el repo y devuelve errores si alguno de los valores no coincide;
+    public void login(ModelMap modelo, String password, String alias) throws ErrorServicio{
+
+        Usuario usuario = usuarioRepositorio.buscarPorMailoAlias(alias);
+        if (usuario != null){
+            if (password.equals(usuario.getContraseña())){
+                System.out.println("LOGIN EXITOSO");
+                modelo.put("Usuario", usuario);
+            } else {
+                throw new ErrorServicio("Contraseña incorrecta");
+            }
+            
+        } else {
+            throw new ErrorServicio("No existe Usuario con ese Mail o Alias");
+        }
+     {
+            
         }
     }
 }
